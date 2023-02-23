f4cfcc21b104d098d886b64620cb85d049ec1799
Deepak Jangir <deepak@Deepaks-MacBook-Pro.local>
2023-02-23 09:33:11 +0000

    Added a simple QR code url

--- qrdevelopment/urls.py
+++ qrdevelopment/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,re_path
-from .views import generate_qr_with_image
+from .views import generate_qr_with_image,generate_simple_qr
 
 # urlpatterns = [
 #     re_path(r'^generate_qr_with_image/$', generate_qr_with_image, name='generate_qr'),
@@ -8,5 +8,6 @@ from .views import generate_qr_with_image
 
 urlpatterns = [
     path('generate_qr_with_image/', generate_qr_with_image, name='generate_qr_with_image'),
+    path('generate_simple_qr/<str:url>/', generate_simple_qr, name='generate_simple_qr'),
 ]
 
--- qrdevelopment/views.py
+++ qrdevelopment/views.py
@@ -11,54 +11,54 @@ from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.colormasks import ImageColorMask
 
 # 1. Uses File Image in Post request
-# @csrf_exempt
-# def generate_qr_with_image(request):
-#     basewidth = 100
+@csrf_exempt
+def generate_qr_with_image_file(request):
+    basewidth = 100
 
-#     if request.method == 'POST':
-#         url = request.POST.get('url', '')
-#         image_data = request.FILES.get('image', '')
-#         QRcolor = request.POST.get('color', '')
-#         if image_data:
-#             # Load image
-#             image = Image.open(image_data)
+    if request.method == 'POST':
+        url = request.POST.get('url', '')
+        image_data = request.FILES.get('image', '')
+        QRcolor = request.POST.get('color', '')
+        if image_data:
+            # Load image
+            image = Image.open(image_data)
 
-#             wpercent = (basewidth/float(image.size[0]))
-#             hsize = int((float(image.size[1])*float(wpercent)))
-#             logo = image.resize((basewidth, hsize), Image.ANTIALIAS)
+            wpercent = (basewidth/float(image.size[0]))
+            hsize = int((float(image.size[1])*float(wpercent)))
+            logo = image.resize((basewidth, hsize), Image.ANTIALIAS)
 
-#             QRcode = qrcode.QRCode(
-#                 error_correction=qrcode.constants.ERROR_CORRECT_H
-#             )
+            QRcode = qrcode.QRCode(
+                error_correction=qrcode.constants.ERROR_CORRECT_H
+            )
 
-#             # adding URL or text to QRcode
-#             QRcode.add_data(url)
+            # adding URL or text to QRcode
+            QRcode.add_data(url)
 
-#             # generating QR code
-#             QRcode.make()
+            # generating QR code
+            QRcode.make()
 
-#             # taking color name from user
-#             # QRcolor = 'Green'
+            # taking color name from user
+            # QRcolor = 'Green'
 
-#             # adding color to QR code
-#             QRimg = QRcode.make_image(fill_color=QRcolor, back_color="white").convert('RGB')
+            # adding color to QR code
+            QRimg = QRcode.make_image(fill_color=QRcolor, back_color="white").convert('RGB')
 
-#             # set size of QR code
-#             pos = ((QRimg.size[0] - logo.size[0]) // 2,
-#                 (QRimg.size[1] - logo.size[1]) // 2)
-#             QRimg.paste(logo, pos)
+            # set size of QR code
+            pos = ((QRimg.size[0] - logo.size[0]) // 2,
+                (QRimg.size[1] - logo.size[1]) // 2)
+            QRimg.paste(logo, pos)
 
 
-#             # Save image data to buffer
-#             buffer = BytesIO()
-#             QRimg.save(buffer, format='PNG')
-#             image_png = buffer.getvalue()
-#             buffer.close()
+            # Save image data to buffer
+            buffer = BytesIO()
+            QRimg.save(buffer, format='PNG')
+            image_png = buffer.getvalue()
+            buffer.close()
 
-#             # Return image as HTTP response
-#             response = HttpResponse(image_png, content_type="image/png")
-#             return response
-#     return HttpResponse(status=405)
+            # Return image as HTTP response
+            response = HttpResponse(image_png, content_type="image/png")
+            return response
+    return HttpResponse(status=405)
 
 # 2. Uses base64 as the input in post request
 @csrf_exempt
@@ -109,3 +109,16 @@ def generate_qr_with_image(request):
             response = HttpResponse(image_png, content_type="image/png")
             return response
     return HttpResponse(status=405)
+
+
+def generate_simple_qr(request, url):
+    qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+    qr.add_data(url)
+    qr.make()
+    img = qr.make_image(fill_color="black", back_color="white")
+    buffer = BytesIO()
+    img.save(buffer, format='PNG')
+    image_png = buffer.getvalue()
+    buffer.close()
+    response = HttpResponse(image_png, content_type="image/png")
+    return response
